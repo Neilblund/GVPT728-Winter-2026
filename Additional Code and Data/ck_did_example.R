8162dc090e29bfa519581fb4a2d0a80baf5eaceb
Neilblund <neilblund@gmail.com>
2026-01-21 15:02:53 -0500

    CK did update

--- Additional Code and Data/ck_did_example.R
+++ Additional Code and Data/ck_did_example.R
@@ -9,7 +9,8 @@ library(labelled)
 url<-'https://github.com/Neilblund/GVPT728_Winter24/raw/refs/heads/main/Additional%20Code%20and%20Data/ck_longformat.rds'
 file<-tempfile()
 download.file(url, dest=file, mode='wb')
-ckdata<-readRDS(file)
+ckdata<-readRDS(file)|>
+  ungroup()
 
 
 # dv = FTE 
@@ -26,43 +27,39 @@ ggplot(data=ckdata, aes(x=state, y=fte, fill=period)) +
 
 
 
+
 ## Baseline DiD
 model<-lm(fte ~ period  * state, data=ckdata )
 summary(model)
 
 # DID with control for bonus programs for new workers at time 1
 
+
+
 lm(fte ~ period * state + bonus, data=ckdata)|>
   summary()
 
+lm(fte ~state , data=ckdata)|>
+  summary()
 
-# DiD with temporarily closed cases set to zero employement
 
-ckdata$fte_temp_closed<-ifelse(ckdata$status == "closed for renovations", 0, ckdata$fte)
+# DiD with temporarily closed cases in New Jersey set to zero employment
+
+ckdata$fte_temp_closed<-ifelse(ckdata$status %in% c("closed for renovations",
+                                                    "closed due to Mall fire",
+                                                    "closed for highway construction"
+                                                    ) & ckdata$period == "Wave 2"
+                               & ckdata$state == "NJ"
+                               , 0, ckdata$fte)
+
+
 
 
 lm(fte_temp_closed ~ period * state , data=ckdata)|>
   summary()
 
-
-# DiD with all missing data set to zero employment
-
-ckdata$fte_worstcase<-ifelse(is.na(ckdata$fte), 0, ckdata$fte)
-
-lm(fte_worstcase ~ period * state , data=ckdata)|>
+lm(fte_temp_closed ~ period * state + bonus  , data=ckdata)|>
   summary()
-
-
-
-
-
-# looking at actual means under worst case scenario condition
-ckdata|>
-  select(state, period, fte_worstcase)|>
-  group_by(state, period)|>
-  summarise(fte_worstcase = mean(fte_worstcase)
-  )|>
-  pivot_wider(values_from = fte_worstcase, names_from = period)
 
 
 # Accounting for clustering-------
@@ -88,8 +85,22 @@ library(fixest)
 ckdata|>
   mutate(minwage = ifelse(period == "Wave 2" & state=="NJ", "treated", "not treated"))|> 
   feols( fte ~ minwage | period  + state, data=_, 
-       cluster=~sheet)
+       cluster=~sheet)|>
+  broom::tidy()
 
+
+
+
+
+# Equivalent using difference in employment at Wave 2 - Wave 1
+# (note that this setup only really works with panel data because we need to get 
+# the differences for individual cases)
+ckdata|>
+  select(state, sheet,  period, status, fte)|>
+  pivot_wider(names_from = period, values_from =fte)|>
+  mutate(difference = `Wave 2` - `Wave 1`)|>
+  lm(difference ~ state, data=_)|>
+  broom::tidy()
 
 
 
